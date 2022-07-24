0fbd7100095666ffc3d910b9eb57f26f0207efa4
Poulami Basu <98650837+Poulami40@users.noreply.github.com>
2022-07-24 09:35:18 +0530

    Create functionsinc.c

---
+++ functionsinc.c
@@ -0,0 +1,19 @@
+/*Write a function int max_of_four(int a, int b, int c, int d) which reads four arguments and returns the greatest of them.*/
+
+#include <stdio.h>
+/*
+Add `int max_of_four(int a, int b, int c, int d)` here.
+*/
+int max_of_four(int a,int b,int c,int d)
+{
+   
+}
+int main() 
+{
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
